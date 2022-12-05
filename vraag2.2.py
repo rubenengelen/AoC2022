356f07921ea7527d71f055958d80827c298b8a77
Ruben Engelen <ruben.engelen@student.pxl.be>
2022-12-05 10:25:05 +0100

    part 2

---
+++ vraag2.2.py
@@ -0,0 +1,29 @@
+file = open('inputs/vraag2.txt','r')
+score = 0
+for i in file:
+	deel = i.split()
+
+	if deel[1] == "X":
+		if deel[0] == "A":
+			score = score + 3
+		elif deel[0] == "B":
+			score = score + 1
+		elif deel[0] == "C":
+			score = score + 2
+	elif deel[1] == "Y":
+		score = score + 3
+		if deel[0] == "A":
+			score = score + 1
+		elif deel[0] == "B":
+			score = score + 2
+		elif deel[0] == "C":
+			score = score + 3
+	elif deel[1] == "Z":
+		score = score + 6
+		if deel[0] == "A":
+			score = score + 2
+		elif deel[0] == "B":
+			score = score + 3
+		elif deel[0] == "C":
+			score = score + 1
+print(score)
